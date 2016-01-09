d79dc5827eadfacb2f326165059b17e308ecb08b
Pavitar Sidhu <pav_sidhu@live.ca>
2016-01-08 18:45:18 -0800

    Name correction.

---
+++ Dice Roller/DiceRoll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DiceRoll
+//
+//  Created by Pavitar Sidhu on 2015-04-25.
+//  Copyright (c) 2015 Vimzy App Productions Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dice Roller/DiceRoll/UIView+DieView.m
@@ -0,0 +1,13 @@
+//
+//  UIView+DieView.m
+//  DiceRoll
+//
+//  Created by Pavitar Sidhu on 2015-04-25.
+//  Copyright (c) 2015 Vimzy App Productions Inc. All rights reserved.
+//
+
+#import "UIView+DieView.h"
+
+@implementation UIView (DieView)
+
+@end
